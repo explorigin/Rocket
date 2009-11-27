9f39f9377cffc6c96c63039bdafc1ce1a057bae0
Timothy Farrell <tfarrell@owassobible.org>
2009-11-27 13:09:10 -0600

    Added setup_utils script and rearranged code to accommodate it.

---
+++ setup.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import re
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+def find_packages(dir_):
+    packages = []
+    for _dir, subdirectories, files in os.walk(os.path.join(dir_, 'rocket')):
+        if '__init__.py' in files:
+            lib, fragment = _dir.split(os.sep, 1)
+            packages.append(fragment.replace(os.sep, '.'))
+    return packages
+
+
+if sys.version_info < (2, 5):
+    raise Exception("Rocket requires Python 2.5 or higher.")
+
+v = open(os.path.join(os.path.dirname(__file__), 'src', 'rocket', '__init__.py'))
+VERSION = re.compile(r".*VERSION = '(.*?)'", re.S).match(v.read()).group(1)
+v.close()
+
+setup(name = "Rocket",
+      version = VERSION,
+      description = "Multi-threaded, Comet-Friendly HTTP Server",
+      author = "Timothy Farrell",
+      author_email = "tfarrell@owassobible.org",
+      url = "http://www.launchpad.net/rocket",
+      packages = find_packages('src'),
+      package_dir = {'':'src'},
+      license = "MIT License",
+      long_description = """\
+""",
+      classifiers = [
+        "Development Status :: 1 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        ]
+      )
