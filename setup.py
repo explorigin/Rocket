5ce0cc62d878d1cb812bf1acfdfec60b2869d03e
Timothy Farrell <tfarrell@owassobible.org>
2009-12-02 10:56:14 -0600

    Move from using setuptools to distribute in order to better support Python 3. Moved the source code around again to better facilitate development.

--- setup.py
+++ setup.py
@@ -1,24 +1,15 @@
+from distribute_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, find_packages
+
 import os
 import sys
 import re
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-def find_packages(dir_):
-    packages = []
-    for _dir, subdirectories, files in os.walk(os.path.join(dir_, 'rocket')):
-        if '__init__.py' in files:
-            lib, fragment = _dir.split(os.sep, 1)
-            packages.append(fragment.replace(os.sep, '.'))
-    return packages
-
 
 if sys.version_info < (2, 5):
     raise Exception("Rocket requires Python 2.5 or higher.")
 
-v = open(os.path.join(os.path.dirname(__file__), 'src', 'rocket', '__init__.py'))
+v = open(os.path.join(os.path.dirname(__file__), 'rocket', '__init__.py'))
 VERSION = re.compile(r".*VERSION = '(.*?)'", re.S).match(v.read()).group(1)
 v.close()
 
@@ -28,8 +19,7 @@ setup(name = "Rocket",
       author = "Timothy Farrell",
       author_email = "tfarrell@owassobible.org",
       url = "http://www.launchpad.net/rocket",
-      packages = find_packages('src'),
-      package_dir = {'':'src'},
+      packages = find_packages(),
       license = "MIT License",
       long_description = "",
       classifiers = [
