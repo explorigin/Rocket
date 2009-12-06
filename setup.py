8ace0e7eade28a1be8a7f56dc7050a8b74dda465
Timothy Farrell <tfarrell@owassobible.org>
2009-12-05 20:10:49 -0600

    Update long-description in preparation for the cheeseshop.

--- setup.py
+++ setup.py
@@ -20,13 +20,16 @@ v.close()
 
 setup(name = "Rocket",
       version = VERSION,
-      description = "Multi-threaded, Comet-Friendly HTTP Server",
+      description = "Modern, Multi-threaded, Comet-Friendly WSGI Web Server",
       author = "Timothy Farrell",
       author_email = "tfarrell@owassobible.org",
       url = "http://www.launchpad.net/rocket",
       packages = find_packages(),
       license = "MIT License",
-      long_description = "",
+      long_description = """The Rocket web server is a pure Python server designed to handle the increased needs of modern web applications.  It is designed to be small and simple enough to be bundled with other Python projects while remaining performant enough to handle production work loads.  Currently Rocket is limited to serving WSGI applications and middleware.
+      
+      Rocket runs on cPython 2.5-3.x and Jython 2.5 (without the need to run through the 2to3 translation tool).
+      """,
       classifiers = [
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
