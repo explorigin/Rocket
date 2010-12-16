aa67c461ec289a5fef066ecda269a5dab2c684b9
Timothy Farrell <tfarrell@owassobible.org>
2010-12-16 03:19:48 -0600

    Fixed test_listener.py for HTTPS connection tests.

--- tests/test_listener.py
+++ tests/test_listener.py
@@ -6,6 +6,7 @@
 # See the included LICENSE.txt file for licensing details.
 
 # Import System Modules
+import os
 import time
 import socket
 import unittest
@@ -24,12 +25,20 @@ except ImportError:
 from rocket import listener
 
 # Constants
+PRIV_KEY_FILE = os.path.join(os.path.dirname(__file__), "priv_key.pem")
+PUB_KEY_FILE = os.path.join(os.path.dirname(__file__), "pub_key.pem")
+
 
 # Define Tests
 class ListenerTest(unittest.TestCase):
     def setUp(self):
         self.active_queue = Queue()
         self.interface = ("127.0.0.1", 45451)
+        if has_ssl:
+            self.secure_interface = ("127.0.0.1",
+                                     45452,
+                                     PRIV_KEY_FILE,
+                                     PUB_KEY_FILE)
 
     def testReady(self):
         self.listener = listener.Listener(self.interface,
@@ -38,34 +47,45 @@ class ListenerTest(unittest.TestCase):
 
         self.assert_(self.listener.ready)
 
+        if has_ssl:
+            self.assert_(os.path.exists(PRIV_KEY_FILE),
+                         msg="Could not find private key file: "+ os.path.abspath(PRIV_KEY_FILE))
+            self.assert_(os.path.exists(PUB_KEY_FILE),
+                         msg="Could not find public key file: "+ os.path.abspath(PUB_KEY_FILE))
+
+            self.sec_listener = listener.Listener(self.secure_interface,
+                                                  5,
+                                                  self.active_queue)
+            self.assert_(self.sec_listener.ready)
+
     def testNotReady(self):
         self.testReady() # create Listener
-       
+
         self.listener.ready = False
-        
+
         self.listener.start()
-        
+
         # Give thread a chance to die
         time.sleep(0.5)
-        
+
         self.assert_(not self.listener.isAlive())
-        
+
     def testListen(self):
         self.testReady() # create Listener
         self.listener.start()
-        
+
         self.assert_(self.listener.isAlive())
-        
+
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
+
         sock.settimeout(5)
-        
+
         sock = sock.connect(self.interface)
-        
+
         time.sleep(0.5)
-        
+
         self.assertEqual(self.listener.active_queue.qsize(), 1)
-        
+
     def testWrapSocket(self):
         if not has_ssl:
             print "ssl module not available"
@@ -73,12 +93,13 @@ class ListenerTest(unittest.TestCase):
 
         self.testReady() # create Listener
 
-        self.assert_(self.listener.secure, msg="must test on an HTTPS socket")
+        self.assert_(self.sec_listener.secure,
+                     msg="must test on an HTTPS socket")
 
-        sock, client = self.listener.wrap_socket((self.listener.listener, None))
-        
+        sock, client = self.sec_listener.wrap_socket((self.sec_listener.listener, None))
+
         self.assert_(isinstance(sock, ssl.SSLSocket))
-        
+
     def tearDown(self):
         try:
             self.listener.ready = False
@@ -86,7 +107,7 @@ class ListenerTest(unittest.TestCase):
             self.assert_(not self.listener.isAlive())
         except:
             pass
-    
+
         try:
             del self.listener
         except:
