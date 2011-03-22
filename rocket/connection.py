edebaef314e40fdee39fcbd02a77af30176d147f
Timothy Farrell <tfarrell@owassobible.org>
2011-03-21 20:27:58 -0500

    FileLikeSocket is not ready for prime-time.  Leave the code for experimentation but commented out.

--- rocket/connection.py
+++ rocket/connection.py
@@ -12,15 +12,10 @@ try:
     has_ssl = True
 except ImportError:
     has_ssl = False
-try:
-    from io import StringIO
-except ImportError:
-    try:
-        from cStringIO import StringIO
-    except ImportError:
-        from StringIO import StringIO
-
+# Import Package Modules
 from . import IS_JYTHON, SOCKET_TIMEOUT, BUF_SIZE, b
+# TODO - This part is still very experimental.
+#from .filelike import FileLikeSocket
 
 class Connection(object):
     __slots__ = [
@@ -64,9 +59,11 @@ class Connection(object):
         self.setblocking = self.socket.setblocking
         self.recv = self.socket.recv
         self.send = self.socket.send
+        self.makefile = self.socket.makefile
 
-    def makefile(self, buf_size=BUF_SIZE):
-        return FileLikeSocket(self, buf_size)
+# FIXME - this is not ready for prime-time yet.
+#    def makefile(self, buf_size=BUF_SIZE):
+#        return FileLikeSocket(self, buf_size)
 
     def close(self):
         if hasattr(self.socket, '_sock'):
@@ -80,110 +77,3 @@ class Connection(object):
                     pass
         self.socket.close()
 
-class FileLikeSocket(object):
-    def __init__(self, conn, buf_size=BUF_SIZE):
-        self.conn = conn
-        self.buf_size = buf_size
-        self.buffer = StringIO()
-        self.content_length = None
-
-        if self.conn.socket.gettimeout() == 0.0:
-            self.read = self.non_blocking_read
-        else:
-            self.read = self.blocking_read
-
-    def __iter__(self):
-        return self
-
-    def recv(self, size):
-        while True:
-            try:
-                return self.conn.recv(size)
-            except socket.error:
-                exc = sys.exc_info()
-                e = exc[1]
-                # FIXME - Don't raise socket_errors_nonblocking or socket_error_eintr
-                if (e.args[0] not in set()):
-                    raise
-
-    def next(self):
-        data = self.readline()
-        if data == '':
-            raise StopIteration
-        return data
-
-    def non_blocking_read(self, size=None):
-        # Shamelessly adapted from Cherrypy!
-        bufr = self.buffer
-        bufr.seek(0, 2)
-        if size is None:
-            while True:
-                data = self.recv(self.buf_size)
-                if not data:
-                    break
-                bufr.write(data)
-
-            self.buffer = StringIO()
-
-            return bufr.getvalue()
-        else:
-            buf_len = self.buffer.tell()
-            if buf_len >= size:
-                bufr.seek(0)
-                data = bufr.read(size)
-                self.buffer = StringIO(bufr.read())
-                return data
-
-            self.buffer = StringIO()
-            while True:
-                remaining = size - buf_len
-                data = self.recv(remaining)
-
-                if not data:
-                    break
-
-                n = len(data)
-                if n == size and not buf_len:
-                    return data
-
-                if n == remaining:
-                    bufr.write(data)
-                    del data
-                    break
-
-                bufr.write(data)
-                buf_len += n
-                del data
-
-            return bufr.getvalue()
-
-    def blocking_read(self, length=None):
-        if length is None:
-            if self.content_length is not None:
-                length = self.content_length
-            else:
-                length = 1
-
-        try:
-            data = self.conn.recv(length)
-        except:
-            data = b('')
-
-        return data
-
-    def readline(self):
-        data = b("")
-        char = self.read(1)
-        while char != b('\n') and char is not b(''):
-            line = repr(char)
-            data += char
-            char = self.read(1)
-        data += char
-        return data
-
-    def readlines(self, hint="ignored"):
-        return list(self)
-
-    def close(self):
-        self.conn = None
-        self.content_length = None
